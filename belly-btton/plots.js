103805cb8b1d506e87e1de9775ba490eb3f7b943
Mahesh D Nair <mahesh.nair@sky.uk>
2020-02-23 17:52:49 -0500

    Added the card on HTML

--- belly-btton/plots.js
+++ belly-btton/plots.js
@@ -1,44 +1,3 @@
-// // First function to call and populate when page loads
-
-// function init(){
-//    d3.json("data/samples.json").then((data) => {
-//       //console.log(data.samples[0].sample_values)
-//       //sorted_values=data.samples[0].sample_values.sort((a,b) => b.data.samples[0].sample_values - a.data.samples[0].sample_values)
-      
-//       //console.log(sorted_values)
-//       values=data.samples[0].sample_values
-//       sort_array = values.sort((a,b) => b - a)
-//       x_array=sort_array.slice(0,10).reverse()
-
-//       y_array = []
-//       for (i=0;i<10;i++){
-//          y_array.push("UT"+data.samples[0].otu_ids[i])
-//       }
-     
-//       text_array=data.samples[0].otu_labels.slice(0,10)
-//       y_array = y_array.reverse()
-//       trace1={
-//          x:x_array,
-//          y:y_array,
-//          text:text_array,
-//          orientation: 'h',
-//          type: "bar"
-//       }
-   
-//       var layout = {
-//          title: "Belly Button",
-//          margin: {
-//            l: 200,
-//            r: 400,
-//            t: 50,
-//            b: 50
-//          }
-//        };
-//       data=[trace1]
-//       Plotly.newPlot("plot", data,layout);
-   
-//    })
-// }
 // Populate drop down
 
 id=[]
@@ -103,7 +62,6 @@ function getData(){
                   }
          
             var layout = {
-               title: "Belly Button",
                margin: {
                  l: 50,
                  r: 50,
@@ -155,65 +113,37 @@ function getData(){
 
    data.metadata.forEach(function(key,){
 
-      
-
       if (dataset == key["id"]){
 
-         console.log("Inside check in gauge" + key.id)
-      var traceGauge = {
-         type: 'pie',
-         showlegend: false,
-         hole: 0.4,
-         rotation: 90,
-         values: [ 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180],
-         text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
-         direction: 'clockwise',
-         textinfo: 'text',
-         textposition: 'inside',
-         marker: {
-            colors: ['','','','','','','','','','white'],
-            labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9']
-         }
-      }
-
-    // needle
-      var degrees = 50, radius = .9
-      var radians = degrees * Math.PI / 180
-      console.log("Wash Freq"+key.wfreq)
-      var x = -1 * radius * Math.cos(radians) * key.wfreq
-      console.log("From gauge x"+ x)
-      var y = radius * Math.sin(radians)
-      final_freq = 0.1*key.wfreq+0.2
-      console.log("Final Freq"+final_freq)
-      console.log("From gauge y"+ y)
-      var gaugeLayout = {
-         shapes: [{
-         type: 'path',
-         path: `M 0.5 0.5 L ${final_freq} 0.55 L 0.55 0.5 Z`,
-         'fillcolor': 'rgba(44, 160, 101, 0.5)',
-         line: {
-             color: 'black',
-            width: 3
-          }
-         }],
-         title: 'Chart',
-       //  xaxis: {visible: false, range: [-1, 1]},
-        // yaxis: {visible: false, range: [-1, 1]}
-      }
-
-      var dataGauge = [traceGauge]
-
-      Plotly.plot('plot2', dataGauge, gaugeLayout)
-
+         var chart = am4core.create("plot2", am4charts.GaugeChart);
+         var axis = chart.xAxes.push(new am4charts.ValueAxis());
+         axis.min = 0;
+         axis.max = 9;
+         axis.strictMinMax = true;
+         chart.innerRadius = -20;
+         var range = axis.axisRanges.create();
+         range.value = 0;
+         range.endValue = 5;
+         range.axisFill.fillOpacity = 1;
+         range.axisFill.fill = am4core.color("#88AB75");
+         range.axisFill.zIndex = -1;
+         var range = axis.axisRanges.create();
+         range.value = 5;
+         range.endValue = 7;
+         range.axisFill.fillOpacity = 1;
+         range.axisFill.fill = am4core.color("#8BBBBB");
+         range.axisFill.zIndex = -1;
+         var range = axis.axisRanges.create();
+         range.value = 7;
+         range.endValue = 9;
+         range.axisFill.fillOpacity = 1;
+         range.axisFill.fill = am4core.color("#AAABBB");
+         range.axisFill.zIndex = -1;
+         var hand = chart.hands.push(new am4charts.ClockHand());
+         hand.value = key.wfreq
      
    }
 })
 })
 }
-   
-
-   
-
-
-//init()
 
